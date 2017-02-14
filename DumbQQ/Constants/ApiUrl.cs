d69ec78010aad6a3e252ff032e713ec6ebb20e2b
Todd Justin York <tjysunset@outlook.com>
2017-02-14 22:45:31 +0800

    Fixed GetQQById's referer
    Details: https://github.com/ScienJus/smartqq/issues/62

--- DumbQQ/Constants/ApiUrl.cs
+++ DumbQQ/Constants/ApiUrl.cs
@@ -101,7 +101,7 @@ namespace DumbQQ.Constants
 
         public static readonly ApiUrl GetQQById = new ApiUrl(
             "http://s.web2.qq.com/api/get_friend_uin2?tuin={1}&type=1&vfwebqq={2}&t=0.1",
-            "http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1"
+            "http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2"
         );
 
         public static readonly ApiUrl GetDiscussionInfo = new ApiUrl(
